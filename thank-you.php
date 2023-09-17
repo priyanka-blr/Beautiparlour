b8d1bc6b93272ec1e48a5d7c7e640e8df766e49b
sai <sai3cs@gmail.com>
2023-09-17 13:59:28 +0000

    modified

--- thank-you.php
+++ thank-you.php
@@ -6,7 +6,7 @@ error_reporting(0);
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>BPMS-Thank You</title>
+    <title>BPMS-THANK YOU</title>
     
     
     <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
